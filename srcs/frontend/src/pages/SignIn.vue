e2947db0d1f1b6d32f52c859907bea3ab5b6fc58
Oksana Hnatiuk <o.zahakailo@gmail.com>
2026-01-08 13:35:53 +0100

    Initial frontend project

---
+++ srcs/frontend/src/pages/SignIn.vue
@@ -0,0 +1,27 @@
+<template>
+  <BCard title="Sign In" style="max-width: 40rem">
+    <BForm @submit="onSubmit">
+      <BFormFloatingLabel label="Email address" label-for="floatingEmail" class="my-2">
+        <BFormInput id="floatingEmail" type="email"  v-model="form.email" placeholder="Email address" />
+      </BFormFloatingLabel>
+      <BFormFloatingLabel label="Password" label-for="floatingPassword" class="my-2">
+        <BFormInput id="floatingPassword" type="password" v-model="form.password" placeholder="Password" />
+      </BFormFloatingLabel>
+      <BButton type="submit" variant="primary" size="lg"> Sign In</BButton>
+    </BForm>
+  </BCard>
+</template>
+
+<script setup>
+  import {reactive} from 'vue'
+  const form = reactive({
+  email: '',
+  password: ''
+})
+
+const onSubmit = (event) => {
+  event.preventDefault()
+  // eslint-disable-next-line no-alert
+  alert(JSON.stringify(form))
+}
+</script>
---
+++ srcs/frontend/src/router/index.js
@@ -0,0 +1,13 @@
+import SignIn from '@/pages/SignIn.vue'
+import Home from '@/pages/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/signin', component: SignIn },
+  ],
+})
+
+export default router
