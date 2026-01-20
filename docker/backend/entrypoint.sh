8f4dbd2cf822b67a3f2fc5d3924422e2e6657b23
cthaler <cthaler@debian.cthaler.42.fr>
2026-01-20 13:58:46 +0000

    fixed error in entrypoint.sh, changed paths in backend Dockerfile, syntax error in compose file

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd build
-node server.js
 npm ci --omit="dev"
-cat /home/node/app/.env >> /home/node/app/build/.env
+cat /home/node/app/.env.example >> /home/node/app/build/.env
 node bin/server.js
