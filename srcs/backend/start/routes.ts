56aa584724efd9a01932f1d4b012c7ebe4539b92
Hüsrev Akbas <huakbas@c3r6p4.42vienna.com>
2026-01-21 12:39:12 +0100

    ADD: router files divided into many files under /app/routes. It has cleaner structure and will cause less conflicts

--- srcs/backend/start/routes.ts
+++ srcs/backend/start/routes.ts
@@ -7,43 +7,11 @@
 |
 */
 
-const TestusersController = () => import('#controllers/testusers_controller')
-const AuthController = () => import('#controllers/auth_controller')
 import router from '@adonisjs/core/services/router'
-import { middleware } from './kernel.js'
+import '../app/routes/swagger.js'
+import '../app/routes/auth.js'
+import '../app/routes/testuser.js'
 
 router.get('/', async () => {
   return 'Welcome to ft_trancenders secret vault :D'
-})
-
-router.group(() => {
-  router.post('/register', [AuthController, 'register'])
-  router.post('/login', [AuthController, 'login'])
-  router.get('/index', [AuthController, 'index'])
-})
-
-router
-  .group(() => {
-    router.get('/', [TestusersController, 'index'])
-    router.post('/', [TestusersController, 'store'])
-    router.put('/', [TestusersController, 'update'])
-    router.get('/:id', [TestusersController, 'show'])
-    router.delete('/:id', [TestusersController, 'destroy'])
-  })
-  .prefix('/testuser')
-  .use(middleware.auth())
-
-// SWAGGER CONFIGURATION
-import AutoSwagger from 'adonis-autoswagger'
-import swagger from '#config/swagger'
-// returns swagger in YAML
-router.get('/swagger', async () => {
-  return AutoSwagger.default.docs(router.toJSON(), swagger)
-})
-
-// Renders Swagger-UI and passes YAML-output of /swagger
-router.get('/docs', async () => {
-  return AutoSwagger.default.ui('/swagger', swagger)
-  // return AutoSwagger.default.scalar('/swagger') //; to use Scalar instead. If you want, you can pass proxy url as second argument here.
-  // return AutoSwagger.default.rapidoc('/swagger', 'view') //; to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
 })
