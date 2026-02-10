b19c7f25bbe66808256526b63ea7411b74c52cfe
Hüsrev Akbas <huakbas@c3r6p4.42vienna.com>
2026-02-10 14:08:31 +0100

    FEAT: migration for friends added UPDATE: japa/runner version decrased to 3.1.4 to make it compatible with node 22.22.0

---
+++ srcs/backend/database/migrations/1770728616496_create_friends_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'friends'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.integer('user_id').references('users.id')
+      table.integer('friend_id').references('users.id')
+      table.unique(['user_id', 'friend_id'])
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
