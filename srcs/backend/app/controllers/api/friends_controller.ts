646aeaa0f7b3d9904a3b5b03cb1807b96f0362fe
Hüsrev Akbas <huakbas@c3r6p4.42vienna.com>
2026-02-10 18:34:34 +0100

    FEAT: user add remove implemented without request/pending options

--- srcs/backend/app/controllers/api/friends_controller.ts
+++ srcs/backend/app/controllers/api/friends_controller.ts
@@ -1,5 +1,5 @@
 import User from '#models/user'
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 
 /*
     await user.related('friends').attach({
@@ -10,18 +10,66 @@ import User from '#models/user'
 */
 
 export default class FriendsController {
-  async index() {
-    const user = await User.query().preload('friends').where('id', 1).first()
-    console.log(user?.serialize())
-    console.log('got request for friends list')
-    return user?.serialize()
+  async index({ auth }: HttpContext) {
+    const user = auth.user as User
+    const friends = await user.related('friends').query()
+    // const userWithFriends = await User.query()
+    //   .preload('friends')
+    //   .where('id', user.id)
+    //   .where('friends.status', 'pending')
+    //   .first()
+    // return userWithFriends?.serialize().friends
+    return friends
   }
 
-  async addUser() {
-    const user1 = await User.query().preload('friends').where('id', 1).first()
-    await user1?.related('friends').detach([3])
-    console.log(user1?.serialize().friends)
-    // await user1?.save()
-    return user1?.serialize()
+  /**
+   * @addFriend
+   * @tag friends
+   * @description Add a friend
+   * @requestBody <{ "friendId": "1" }>
+   */
+  async addFriend({ request, response, auth }: HttpContext) {
+    const data = request.only(['friendId'])
+    console.log(data, typeof data.friendId)
+    const authenticatedUser = auth.user as User
+    const user = await User.find(authenticatedUser.id)
+    const friendCheck = await user
+      ?.related('friends')
+      .query()
+      .where('friend_id', data.friendId)
+      .first()
+    if (!user) return response.status(318).send({ errors: [{ messages: 'User not found' }] })
+    if (friendCheck) return response.status(333).send({ errors: [{ messages: 'Already friends' }] })
+    const friend = await User.find(data.friendId)
+    if (!friend) return response.status(333).send({ errors: [{ messages: 'Friend not found' }] })
+    user.related('friends').attach({
+      [Number(data.friendId)]: {
+        status: 'pending',
+      },
+    })
+    friend.related('friends').attach([user.id])
+    return user.related('friends').query()
+  }
+
+  /**
+   * @removeFriend
+   * @tag friends
+   * @description Remove a user from friends list
+   * @requestBody <{ "friendId": "1" }>
+   */
+  async removeFriend({ request, response, auth }: HttpContext) {
+    const data = request.params()
+    const authenticatedUser = auth.user as User
+    const user = await User.find(authenticatedUser.id)
+    const friend = await user
+      ?.related('friends')
+      .query()
+      .where('friend_id', Number(data.friendId))
+      .first()
+    if (!user || !friend)
+      return response.status(318).send({ errors: [{ messages: 'Users are not friends' }] })
+    await user.related('friends').detach([Number(data.friendId)])
+    await friend.related('friends').detach([user.id])
+    return friend.serialize()
   }
 }
--- srcs/backend/app/routes/friend.ts
+++ srcs/backend/app/routes/friend.ts
@@ -1,15 +1,18 @@
-// import { middleware } from '#start/kernel'
+import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
 const FriendsController = () => import('#controllers/api/friends_controller')
 
 router
   .group(() => {
-    router.get('/:id', [FriendsController, 'addUser'])
     router.get('/', [FriendsController, 'index'])
+    // router.get('/:id', [FriendsController, 'addUser'])
+    router.post('/', [FriendsController, 'addFriend'])
+    router.delete('/:friendId', [FriendsController, 'removeFriend'])
     // router.post('/', [TestusersController, 'store'])
     // router.put('/', [TestusersController, 'update'])
     // router.get('/:id', [TestusersController, 'show'])
     // router.delete('/:id', [TestusersController, 'destroy'])
   })
   .prefix('/api/friends')
+  .use(middleware.auth())
