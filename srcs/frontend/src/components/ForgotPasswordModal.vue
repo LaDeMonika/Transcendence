0db269f25dc8eeb89b08a6c64008e4bbbdcd88be
Oksana Hnatiuk <o.zahakailo@gmail.com>
2026-02-22 16:31:22 +0100

    ADD: ForgotPasswordModal and ResetPasswordModal

---
+++ srcs/frontend/src/components/ForgotPasswordModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <BButton variant="link" @click="forgotPassModal = !forgotPassModal"> Forgot your password? </BButton>
+    <BModal
+      v-model="forgotPassModal"
+      title="Forgot Password?"
+    >
+      <span>Enter your email to receive a reset code</span>
+      <BFormFloatingLabel label="Email address" label-for="floatingEmail" class="my-2">
+        <BFormInput id="floatingEmail" type="email" required v-model="form.email" placeholder="Email address" />
+      </BFormFloatingLabel>
+      <template #footer>
+        <BButton type="submit" pill variant="primary" @click="onSubmit">Send Reset Code</BButton>
+      </template>
+    </BModal>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {reactive, ref} from 'vue'
+  const form = reactive({
+  email: ''
+})
+
+  const forgotPassModal = ref(false)
+
+  const emit = defineEmits(['show-reset-password-modal'])
+
+  const onSubmit = () => {
+    forgotPassModal.value = false
+    emit('show-reset-password-modal')
+  }
+</script>
+
+<style scoped>
+.modal-header > h5 {
+  width: 100%;
+}
+</style>
---
+++ srcs/frontend/src/components/ResetPasswordModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <BModal
+    :model-value="props.modelValue"
+    @update:model-value="$emit('update:model-value', $event)"
+    title="Reset Password"
+  >
+    <BFormFloatingLabel label="Reset Code" label-for="resetCode" class="my-2">
+      <BFormInput id="resetCode" type="text" required v-model="form.reset_code" placeholder="Reset Code" />
+    </BFormFloatingLabel>
+    <BFormFloatingLabel label="New Password" label-for="newPassword" class="my-2">
+      <BFormInput id="newPassword" type="password" required v-model="form.password" placeholder="New Password" />
+    </BFormFloatingLabel>
+    <BFormFloatingLabel label="Confirm New Password" label-for="confirmNewPassword" class="my-2">
+      <BFormInput id="confirmNewPassword" type="password" required v-model="form.confirm_password" placeholder="Confirm New Password" />
+    </BFormFloatingLabel>
+    <template #footer>
+      <BButton type="submit" pill variant="primary">Reset Password</BButton>
+    </template>
+  </BModal>
+</template>
+
+<script setup lang="ts">
+import {reactive} from 'vue'
+  const form = reactive({
+  reset_code: '',
+  password: '',
+  confirm_password: ''
+})
+
+const props = defineProps(['modelValue'])
+</script>
+
+<style scoped>
+.modal-header > h5 {
+  width: 100%;
+}
+</style>
