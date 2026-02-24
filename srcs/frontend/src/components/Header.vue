62bde8510ad6f0f10467abfb1723af3ca7277e9e
msimic <lademonic00@gmail.com>
2026-02-24 17:17:23 +0100

    ADD: navbar setup

--- srcs/frontend/src/components/Header.vue
+++ srcs/frontend/src/components/Header.vue
@@ -1,3 +1,27 @@
 <template>
-  PROJECT
+  <!-- This Nav bar is only for landing page - need some ajustment for the rest -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="col-sm d-flex justify-content-start">
+        <a class="navbar-brand" href="#">Logo here</a>
+      </div>
+
+      <div class="col-sm d-flex justify-content-center collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-sm d-flex justify-content-end">
+        <form class="form-inline">
+          <button class="btn btn-outline-success" type="button">Login/Signup</button>
+        </form>
+      </div>
+    </div>
+  </nav>
 </template>
