516a58113a6ad58bf27a352dbddd83ffb4fa37a3
Oksana Hnatiuk <o.zahakailo@gmail.com>
2026-03-17 07:03:34 +0100

    ADD: chat services, load chats list

--- srcs/frontend/src/components/chat/ChatList.vue
+++ srcs/frontend/src/components/chat/ChatList.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>ChatListModal</div>
+  <div>
+    {{ conversations }}
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { chatService } from '@/services/chat.js'
+
+const conversations = ref([])
+const loading = ref(true)
+const error = ref(null)
+
+const loadConversations = async () => {
+  try {
+    loading.value = true
+    error.value = null
+    const data = await chatService.getConversations()
+    conversations.value = data
+  } catch (err) {
+    error.value = 'Failed to load conversations: ' + err.message
+    console.error('Error loading conversations:', err)
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(() => {
+  loadConversations()
+})
+</script>
+
+<style scoped>
+</style>
--- srcs/frontend/src/pages/Chat.vue
+++ srcs/frontend/src/pages/Chat.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="row w-100">
     <div class="col-3">
-      <ChatList />
+      <div class="p-3">
+        <BButton class="mb-3" @click="showCreateModal = true">
+          New Chat
+        </BButton>
+        <ChatList />
+      </div>
     </div>
     <div class="col-9">
       <MessageList />
       <MessageForm />
     </div>
   </div>
+
+  <ChatCreateModal v-if="showCreateModal" @close="showCreateModal = false" />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import ChatCreateModal from "@/components/chat/ChatCreateModal.vue";
 import ChatEditModal from "@/components/chat/ChatEditModal.vue";
 import ChatList from "@/components/chat/ChatList.vue";
 import MessageForm from "@/components/chat/MessageForm.vue";
 import MessageList from "@/components/chat/MessageList.vue";
 
+const showCreateModal = ref(false)
 </script>
 <style scoped>
 
