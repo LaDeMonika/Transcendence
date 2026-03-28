2cffa437570232494f6051f1d44fd3133f5076fe
Oksana Hnatiuk <o.zahakailo@gmail.com>
2026-03-28 19:42:04 +0100

    FEAT: make chat more mobile-friendly

--- srcs/frontend/src/pages/Chat.vue
+++ srcs/frontend/src/pages/Chat.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="row w-100">
+  <div v-if="isMobile" class="w-100">
+    <div v-if="!activeConversation" class="p-3">
+      <BButton class="mb-3" @click="showCreateModal = true">
+        New Chat
+      </BButton>
+      <ChatList ref="chatListRef" :selected-id="activeConversation?.id" @select="openConversation" />
+    </div>
+
+    <div v-else class="p-3 d-flex flex-column">
+      <div class="mb-2 d-flex justify-content-between align-items-center">
+        <BButton size="sm" variant="outline-primary" @click="closeConversation">
+          Back
+        </BButton>
+        <div class="d-flex gap-2">
+          <BButton size="sm" variant="outline-secondary" @click="showMembersModal = true">
+            Manage Users
+          </BButton>
+          <BButton size="sm" variant="outline-danger" :disabled="leaving" @click="leaveConversation">
+            Leave
+          </BButton>
+        </div>
+      </div>
+      <MessageList :conversation-id="activeConversation?.id" :conversation="activeConversation" :key="activeConversation?.id" />
+      <MessageForm :conversation-id="activeConversation?.id" />
+    </div>
+  </div>
+
+  <div v-else class="row w-100">
     <div class="col-3">
       <div class="p-3">
         <BButton class="mb-3" @click="showCreateModal = true">
@@ -15,6 +42,10 @@
         </BButton>
         <BButton size="sm" variant="outline-danger" :disabled="leaving" @click="leaveConversation">
           Leave
+        </BButton>
+
+        <BButton size="sm" variant="outline-primary" @click="closeConversation">
+          Close
         </BButton>
       </div>
 
@@ -43,9 +74,20 @@ const showCreateModal = ref(false)
 const showMembersModal = ref(false)
 const leaving = ref(false)
 const chatListRef = ref(null)
+const isMobile = ref(window.innerWidth < 768)
 
-onMounted(() => connectSocket())
-onUnmounted(() => disconnectSocket())
+const updateIsMobile = () => {
+  isMobile.value = window.innerWidth < 768
+}
+
+onMounted(() => {
+  connectSocket()
+  window.addEventListener('resize', updateIsMobile)
+})
+onUnmounted(() => {
+  disconnectSocket()
+  window.removeEventListener('resize', updateIsMobile)
+})
 
 const activeConversation = ref(null)
 
@@ -72,6 +114,10 @@ const leaveConversation = async () => {
   } finally {
     leaving.value = false
   }
+}
+
+const closeConversation = () => {
+  activeConversation.value = null
 }
 
 </script>
